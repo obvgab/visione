e3a196c987032fccf391a4e527a97c5c29509641
Gab <gabriel@binarysky.ai>
2023-08-14 14:21:02 -0700

    Initial Commit

---
+++ Visione/ContentView.swift
@@ -0,0 +1,22 @@
+////
+//  CAEDE467-6165-4B58-B0C1-DE2A7652E210: 2:20 PM 8/14/23
+//  ContentView.swift by Gab
+//  
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Visione/VisioneApp.swift
@@ -0,0 +1,15 @@
+////
+//  0E3BA8BB-DDEA-4FC7-A21E-CA99FD3F485B: 2:20 PM 8/14/23
+//  VisioneApp.swift by Gab
+//  
+
+import SwiftUI
+
+@main
+struct VisioneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
